56ef85a0b4358a4066f00c262624e8853d0d9ec2
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-07-16 02:22:08 -0700

    add tailwind colors
    add tailwind colors and some html examples

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ function App() {
   return (
     <div>
       <h1>Welcome to your first challange in our team!</h1>
-      <h1>Nugo </h1>
+      <h1 className="text-lightBlue">Nugo</h1>
+      <input
+        type="text"
+        placeholder="Enter your text here"
+        className="p-2 bg-input-lightGrey border border-input-mediumGrey text-inputText"
+      />
+      <ul className="list-none">
+        <li className="flex items-center mb-2">
+          <span className="h-2 w-2 rounded-full bg-listSecondary mr-2"></span>
+          List item 1
+        </li>
+      </ul>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        lightBlue: "#bae6fd",
+        mediumBlue: "##0ea5e9",
+        inputText: "#333",
+      },
+
+      backgroundColor: {
+        inputLightGrey: "#ebebeb",
+        inputMediumGrey: "#6f788d",
+
+        listPrimary: "#ff6f80",
+        listSecondary: "#ffea5c",
+        listNotActive: "#b9dbe6",
+      },
+
+      borderColor: {
+        inputLightGrey: "#ebebeb",
+        inputMediumGrey: "#6f788d",
+      },
+    },
   },
   plugins: [],
 };
