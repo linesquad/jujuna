d891114ff1deb0590c935fceb622fdf9df1990be
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-07-16 09:59:13 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
 function App() {
-  return (
-    <div>
-      <div
-        className="bg-gradient-to-bl 
-      from-backgroundColor-purpleStart from-0% 
-      via-backgroundColor-purpleMid via-34% 
-      to-backgroundColor-purpleEnd to-100% 
-      w-auto h-[700px]"
-      >
-        <span className="text-textColor-primary">nugo</span>
-        <br></br>
-        <span className="text-textColor-secondary text-xl">JUJUNA</span>
-      </div>
-    </div>
-  );
+  return <div>Hello World!</div>;
 }
 
 export default App;
