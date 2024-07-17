7a4118d7e659872b05b48fc9a12ac1dc6026eb45
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-17 13:36:52 +0400

    implement georgian font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div>Hello World!!!</div>;
+  return <div className="font-arialgeo">Hello World!!!</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        arialgeo: ['"Arial GEO"', "sans-serif"],
+      },
       colors: {
         backgroundColor: {
           purpleStart: "#A583D1",
