43a5b289a29960931df9385b8e9de650d6adb9a9
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-08-01 16:26:37 +0200

    change mobilear component background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
       <div className="bg-[linear-gradient(249deg,_#A583D1_22.95%,_#724AA4_46.44%,_#1E122E_93.06%)]">
         <Hero />
         <AboutUsComp />
+        <MobileBar />
       </div>
-      <MobileBar />
     </div>
   );
 };
