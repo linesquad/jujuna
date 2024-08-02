888b08c3b28e3ea148d3956158e6cd9ea757843b
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-08-02 18:39:05 +0400

    resolve problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
 import AboutUsComp from "../components/landingComponents/AboutUsComp";
-<<<<<<< HEAD
-import Connect from "../components/landingComponents/Connect";
-=======
 import Bottle from "../components/landingComponents/Bottle";
->>>>>>> 931e8ac1903ba9c3c26798867079f0c5be962b58
 import Hero from "../components/landingComponents/Hero";
 import MobileBar from "../components/landingComponents/MobileBar";
 
@@ -16,11 +12,6 @@ const Home = () => {
         <Bottle />
         <MobileBar />
       </div>
-<<<<<<< HEAD
-      <MobileBar />
-      <Connect />
-=======
->>>>>>> 931e8ac1903ba9c3c26798867079f0c5be962b58
     </div>
   );
 };
