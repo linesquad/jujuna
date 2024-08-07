6481f2e0bf2fe14e9d0a5a42cfe1cc34287428af
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-08-07 22:39:39 +0200

    implement dark mode

--- src/components/cocktailsComponents/CocktailsSidebar.jsx
+++ src/components/cocktailsComponents/CocktailsSidebar.jsx
@@ -1,30 +1,45 @@
+import { useSelector } from "react-redux";
 import downArrow from "/images/downArrow.svg";
+import { getMode } from "../../features/darkModeSlice";
 
 function CocktailsSidebar() {
+  const darkMode = useSelector(getMode);
   return (
-    <div className="w-[305px] h-[764px] bg-[#fff]/30 rounded-[15px] hidden md:block pt-[50px] pr-[25px] pl-[19px]">
+    <div
+      className={`w-[305px] h-[764px] ${
+        darkMode ? "bg-[#55426E]" : "bg-[#fff]/30"
+      }  rounded-[15px] hidden md:block pt-[50px] pr-[25px] pl-[19px]`}
+    >
       <div className="flex flex-col gap-[35px]">
         <div className="flex justify-between items-center">
-          <p className="text-[14px]">ალკოჰოლური</p>
+          <p className={`text-[14px] ${darkMode ? "text-color-primary" : ""}`}>
+            ალკოჰოლური
+          </p>
           <div className="w-[24px] h-[24px] rounded-[50%] bg-[#D9D9D9]"></div>
         </div>
 
         <div className="flex justify-between items-center">
-          <p className="text-[14px]">ქვეყანა</p>
+          <p className={`text-[14px] ${darkMode ? "text-color-primary" : ""}`}>
+            ქვეყანა
+          </p>
           <div className="w-[24px] h-[24px] rounded-[50%] bg-[#fff] flex justify-center items-center">
             <img src={downArrow} alt="down-arrow" />
           </div>
         </div>
 
         <div className="flex justify-between items-center">
-          <p className="text-[14px]">გამოშვების წელი</p>
+          <p className={`text-[14px] ${darkMode ? "text-color-primary" : ""}`}>
+            გამოშვების წელი
+          </p>
           <div className="w-[24px] h-[24px] rounded-[50%] bg-[#fff] flex justify-center items-center">
             <img src={downArrow} alt="down-arrow" />
           </div>
         </div>
 
         <div className="flex justify-between items-center">
-          <p className="text-[14px]">ზომა/მოცულობა (მლ)</p>
+          <p className={`text-[14px] ${darkMode ? "text-color-primary" : ""}`}>
+            ზომა/მოცულობა (მლ)
+          </p>
           <div className="w-[24px] h-[24px] rounded-[50%] bg-[#fff] flex justify-center items-center">
             <img src={downArrow} alt="down-arrow" />
           </div>
--- src/pages/Cocktails.jsx
+++ src/pages/Cocktails.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import CocktailsHeader from "../components/cocktailsComponents/CocktailsHeader";
 import CocktailsSidebar from "../components/cocktailsComponents/CocktailsSidebar";
 import DisplayCocktails from "../components/cocktailsComponents/DisplayCocktails";
 import Wrapper from "../components/Wrapper";
+import { getMode } from "../features/darkModeSlice";
 
 function CockTails() {
+  const darkMode = useSelector(getMode);
   return (
-    <div className="bg-[#eaeaea]">
+    <div className={`${darkMode ? "bg-[#55426E]" : "bg-[#eaeaea]"}`}>
       <CocktailsHeader />
       <Wrapper>
         <div className="flex gap-[40px] mt-[23px] md:mt-[40px] lg:mt-[60px] px-[14px] md:px-[0px] py-[15px]">
