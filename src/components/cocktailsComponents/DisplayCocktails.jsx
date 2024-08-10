ff798e127d84396b560d97c6db877331767ed52d
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-08-10 04:29:10 +0200

    feat: add loader and error handlings

--- src/components/cocktailsComponents/DisplayCocktails.jsx
+++ src/components/cocktailsComponents/DisplayCocktails.jsx
@@ -2,9 +2,20 @@ import useCocktails from "../../hooks/useCocktails";
 import Wrapper from "../Wrapper";
 import CocktailCard from "./CocktailCard";
 import Pegination from "../Pegination";
+import Spinner from "../../components/Spinner";
 
 function DisplayCocktails() {
-  const { data: cocktails } = useCocktails();
+  const { data: cocktails, isLoading, isError, error } = useCocktails();
+
+  if (isLoading) return <Spinner />;
+
+  if (isError)
+    return (
+      <h2 className="md:text-[22px] lg:text-[26px] text-center text-red-700">
+        {error.message}
+      </h2>
+    );
+
   return (
     <Wrapper>
       <div className="grid gap-[16px] grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] m-auto relative">
--- src/pages/CocktailDetails.jsx
+++ src/pages/CocktailDetails.jsx
@@ -4,11 +4,26 @@ import useCocktails from "../hooks/useCocktails";
 import CocktailDetailsCard from "../components/cocktailsComponents/cocktailsDetailsComponents/CocktailDetailsCard";
 import SingleCocktailProduct from "../components/cocktailsComponents/cocktailsDetailsComponents/SingleCocktailProduct";
 import SameCocktails from "../components/cocktailsComponents/cocktailsDetailsComponents/SameCocktails";
+import Spinner from "../components/Spinner";
 
 function CocktailDetails() {
   const { id } = useParams();
-  const { data: cocktails } = useCocktails();
+  const { data: cocktails, isLoading, isError, error } = useCocktails();
   const cocktail = cocktails?.find((item) => String(item.id) === String(id));
+
+  if (isLoading)
+    return (
+      <div className="h-[60vh] flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+
+  if (isError)
+    return (
+      <h2 className="md:text-[22px] lg:text-[26px] text-center text-red-700">
+        {error.message}
+      </h2>
+    );
 
   return (
     <div className="bg-[#eaeaea]">
