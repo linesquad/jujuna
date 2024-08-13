6564a362e515ca6f7076015e5b22dc96b277d0a1
Ctato1 <dznelashvilitato@gmail.com>
2024-08-13 21:06:32 +0400

    add image skeleton

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,19 @@
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <div className="flex gap-4 items-center w-full h-fit bg-[linear-gradient(rgba(185,156,203,1),rgba(174,133,204,1),rgba(138,99,162,1))]">
+        <img
+          src="https://s3-alpha-sig.figma.com/img/a45a/1179/130c40b9ecba6bdd997dd72a3bae6b77?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F7VgPjGJgbPAbQ~eYYzsNVzGDMvOrUVZ4Jx3AyUNAlNxmm98lcNwpCkedn6tRz3F0wQQi0fSq8lCwUil~ybQg3h0PAG8Mh-T7C1ki6TIKy44gpe4T5F6v7lUt0otvYm1kX7Tza17TQF0K2UMqxwoRI~a4N5gZ8QtnylXGecUC~47JHdiphu3V6Mb7ZZ7nZRjDATaD29mTC8FsstUsJK55dZ7Z7ldaNpTXBgkNoSbFuKORcrn67QMjGB85HiBYEOLoeE7ynddsOQCIYnv4sGNcbB0enHRnysqX123ta9q7Wte98hZKBwwhpcAbg8zj0EpqZVggikmgdsOjFtlHC1IQA__"
+          alt="cocktails"
+          className="w-[40%] h-[100vh] bg-bottom object-cover bg-cover "
+        />
+        <div className="w-[300px] h-fit p-3 rounded bg-white">
+            ტესტ
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
 export default SignIn;
