473b851f586c50abad989602360094dc2b40f677
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-08-15 16:14:55 +0200

    fix: fix errors positions

--- src/components/aboutUsComponents/OneInputField.jsx
+++ src/components/aboutUsComponents/OneInputField.jsx
@@ -33,26 +33,33 @@ function OneInputField({
                 pattern: pattern,
               })}
             ></textarea>
-            <p className="self-end">{watch().message?.length || 0} / 500</p>
+            <div className="flex justify-between items-center">
+              <span className="pl-[12px] text-red-600 text-[13px] md:text-[15px]">
+                {error}
+              </span>
+              <p className="self-end">{watch().message?.length || 0} / 500</p>
+            </div>
           </>
         ) : (
-          <input
-            type={inputType}
-            placeholder={placeholder}
-            name={name}
-            className="pl-[16px] py-[10px] text-[13px] w-full [box-shadow:0px_2.657px_2.657px_0px_rgba(0,_0,_0,_0.25)] rounded-[15px] md:w-[100%]"
-            {...register(`${name}`, {
-              required: {
-                value: isRequired,
-                message: errorMessage,
-              },
-              pattern: pattern,
-            })}
-          />
+          <>
+            <input
+              type={inputType}
+              placeholder={placeholder}
+              name={name}
+              className="pl-[16px] py-[10px] text-[13px] w-full [box-shadow:0px_2.657px_2.657px_0px_rgba(0,_0,_0,_0.25)] rounded-[15px] md:w-[100%]"
+              {...register(`${name}`, {
+                required: {
+                  value: isRequired,
+                  message: errorMessage,
+                },
+                pattern: pattern,
+              })}
+            />
+            <span className="pl-[12px] text-red-600 text-[13px] md:text-[15px]">
+              {error}
+            </span>
+          </>
         )}
-        <span className="pl-[12px] text-red-600 text-[13px] md:text-[15px]">
-          {error}
-        </span>
       </div>
     </div>
   );
