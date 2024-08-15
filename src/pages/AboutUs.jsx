86fc677f91cd2ccc36d431fed4a043aa34a05096
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-08-15 20:55:53 +0200

    feat: add darkMode in aboutUs page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
 import ContactInformation from "../components/aboutUsComponents/ContactInformation";
 import Map from "../components/aboutUsComponents/Map";
+import { getMode } from "../features/darkModeSlice";
 
 const AboutUs = () => {
+  const darkMode = useSelector(getMode);
   return (
-    <div className="pb-[36px] bg-[#eaeaea]">
+    <div className={`pb-[36px]  ${darkMode ? "bg-[#55426E]" : "bg-[#eaeaea]"}`}>
       <Map />
       <ContactInformation />
     </div>
