2f62bc4bebf59692c2d1333f66d3409d12f0c7a4
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-08-19 01:12:49 -0700

    pagination

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import BlogDisplay from "../components/newsComponents/BlogDisplay";
 import FilterNews from "../components/newsComponents/FilterNews";
 import HeroNews from "../components/newsComponents/HeroNews";
 import { getMode } from "../features/darkModeSlice";
+import Pegination from "../components/Pegination";
 
 function News() {
   const mode = useSelector(getMode);
@@ -17,6 +18,7 @@ function News() {
       <HeroNews />
       <FilterNews />
       <BlogDisplay />
+      <Pegination pageQuantity={5} />
     </div>
   );
 }
