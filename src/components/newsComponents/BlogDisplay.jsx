8c2ab85376039b5e13d8c020072d00db8f8bf47e
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-08-22 13:14:00 -0700

    add pagination

--- src/components/newsComponents/BlogDisplay.jsx
+++ src/components/newsComponents/BlogDisplay.jsx
@@ -1,10 +1,26 @@
 import BlogWrapper from "./BlogWrapper";
-import useBlogs from "../../hooks/useBlogs";
 import BlogDisplayItems from "./BlogDisplayItems";
 import Spinner from "../Spinner";
+import { useEffect, useState } from "react";
+import { usePaginatedBlogs } from "../../hooks/useBlogs";
 
 const BlogDisplay = () => {
-  const { data: allBlogs = [], isLoading, isError, error } = useBlogs();
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(null);
+
+  const {
+    data: paginatedBlogs = [],
+    isLoading,
+    isError,
+    error,
+  } = usePaginatedBlogs(page, 3);
+
+  useEffect(() => {
+    if (isLoading === false && paginatedBlogs.length === 0) {
+      setPage((prev) => prev - 1);
+      setLastPage(page);
+    }
+  }, [isLoading, paginatedBlogs, page, lastPage]);
 
   if (isLoading) {
     return (
@@ -26,9 +42,25 @@ const BlogDisplay = () => {
     <div className="w-full">
       <BlogWrapper>
         <div className="flex flex-wrap justify-center gap-4 lg:w-full lg:flex-nowrap lg:justify-between lg:flex-col pt-5">
-          {allBlogs.map((item, index) => (
+          {paginatedBlogs.map((item, index) => (
             <BlogDisplayItems item={item} index={index} key={item.id} />
           ))}
+        </div>
+        <div className="flex justify-center mt-4">
+          <button
+            className="bg-purple-500 text-white p-2 m-2 rounded-lg"
+            onClick={() => setPage((prev) => Math.max(prev - 1, 1))} // Ensure page doesn't go below 1
+            disabled={page === 1}
+          >
+            Previous
+          </button>
+          <button
+            className="bg-purple-500 text-white p-2 m-2 rounded-lg"
+            onClick={() => setPage((prev) => prev + 1)}
+            disabled={page === lastPage - 1}
+          >
+            Next
+          </button>
         </div>
       </BlogWrapper>
     </div>
