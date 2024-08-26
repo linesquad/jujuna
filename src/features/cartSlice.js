6c0fe997996128d3b72a7414f96a647a19e5ed31
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-08-26 06:09:50 -0700

    add cartSlice

---
+++ src/features/cartSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: {},
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+});
