2ed6cb2bc8d551e4d1046bc7c165fc221ce3b53c
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-09-11 03:46:52 +0200

    add saleProducts design

--- src/components/landingComponents/SaleProducts.jsx
+++ src/components/landingComponents/SaleProducts.jsx
@@ -1,29 +1,74 @@
+import { useRef } from "react";
 import Wrapper from "../Wrapper";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import SingleSaleProduct from "./SingleSaleProduct";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function SaleProducts() {
+  const swiperRef = useRef(null);
+
   return (
     <div>
       <Wrapper>
         <div className="mt-[100px]">
-          <div className="flex items-start justify-between pl-[50px]">
-            <h2 className="text-[32px] text-[#848282]">
+          <div className="flex items-center justify-between pl-[50px] pb-[12px] border-b-[1px] border-[#D9D9D9]">
+            <h2 className="text-[16px] md:text-[22px] lg:text-[32px] text-[#848282]">
               ფასდაკლებული პროდუქტები
             </h2>
 
-            <div className="flex items-center gap-[4px]">
-              <div className="w-[36px] h-[36px] border-[1px] border-[#BB8DF580] rounded-[50%] flex justify-center items-center">
+            <div className="hidden lg:flex items-center gap-[4px]">
+              <div
+                className="w-[36px] h-[36px] border-[1px] border-[#BB8DF580] rounded-[50%] flex justify-center items-center cursor-pointer"
+                onClick={() => swiperRef.current.slidePrev()}
+              >
                 <FaArrowLeft color="#DCC6FA" />
               </div>
 
-              <div className="w-[36px] h-[36px] border-[1px] border-[#BB8DF580] rounded-[50%] flex justify-center items-center">
+              <div
+                className="w-[36px] h-[36px] border-[1px] border-[#BB8DF580] rounded-[50%] flex justify-center items-center cursor-pointer"
+                onClick={() => swiperRef.current.slideNext()}
+              >
                 <FaArrowRight color="#DCC6FA" />
               </div>
             </div>
           </div>
-          <div className="mt-[32px]">
-            <SingleSaleProduct />
+          <div className="mt-[32px] flex">
+            <Swiper
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              breakpoints={{
+                320: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                700: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                },
+              }}
+            >
+              <SwiperSlide>
+                <SingleSaleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleSaleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleSaleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleSaleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleSaleProduct />
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </Wrapper>
--- src/components/landingComponents/SingleSaleProduct.jsx
+++ src/components/landingComponents/SingleSaleProduct.jsx
@@ -5,15 +5,17 @@ import { FaShoppingCart } from "react-icons/fa";
 
 function SingleSaleProduct() {
   return (
-    <div className="w-[300px]  pt-[4px] pb-[15px] px-[12px] relative">
-      <div className="w-[120px] h-[175px] mx-auto">
+    <div className="xl:w-[280px] lg:w-[240px] md:w-[200px]  pt-[4px] pb-[15px] px-[12px] relative">
+      <div className="xl:w-[120px] lg:w-[100px] xl:h-[175px] lg:h-[150px] mx-auto">
         <img
           src={cocktail}
           alt="product"
           className="w-full h-full object-cover rounded-md"
         />
       </div>
-      <p className="text-[20px] text-center mt-[20px]">ტვიში - თეთრი მშრალი</p>
+      <p className="text-[12px] md:text-[20px] text-center mt-[20px]">
+        ტვიში - თეთრი მშრალი
+      </p>
 
       <div className="mt-[13px] flex items-center gap-[22px]">
         <div className="flex items-center gap-[5px]">
@@ -23,13 +25,24 @@ function SingleSaleProduct() {
         <p className="text-[14px]">+500 view</p>
       </div>
 
-      <div className="mt-[16px] flex items-center justify-between">
+      <div className="mt-[16px] flex items-center justify-between ">
         <div className="flex flex-col gap-[8px]">
-          <p className="text-[#008E28]">გაყიდვაშია</p>
-          <p className="text-[22px]">45.00 ₾</p>
+          <p className="text-[#008E28] text-[12px] md:text-[16px] ">
+            გაყიდვაშია
+          </p>
+          <p className="text-[12px] md:text-[22px]">45.00 ₾</p>
         </div>
 
-        <div>
+        <div className="flex items-center gap-[6px] border-[1px] border-[#eaeaea] rounded-[20px] p-[2px]">
+          <div className="hidden xl:flex items-center gap-[6px]">
+            <span className="w-[20px] h-[20px] flex justify-center items-center bg-[#eaeaea] rounded-[50%] pb-[3px] cursor-pointer">
+              +
+            </span>
+            <p>1</p>
+            <span className="w-[20px] h-[20px] flex justify-center items-center bg-[#eaeaea] rounded-[50%] pb-[3px] cursor-pointer">
+              -
+            </span>
+          </div>
           <div className="w-[36px] h-[36px] bg-[#BB8DF580] rounded-[50%] flex items-center justify-center">
             <FaShoppingCart color="#FFFFFF" />
           </div>
@@ -38,8 +51,11 @@ function SingleSaleProduct() {
 
       <div className="absolute top-0">
         <div className="relative">
-          <FaStar size={70} color="#FC0909" />
-          <p className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%] text-[#fff] text-[20px]">
+          <FaStar
+            className="lg:text-[50px] xl:text-[70px] text-[50px]"
+            color="#FC0909"
+          />
+          <p className="absolute top-[50%] translate-x-[-50%] left-[50%] translate-y-[-50%] text-[#fff] lg:text-[16px] xl:text-[20px]">
             50%
           </p>
         </div>
