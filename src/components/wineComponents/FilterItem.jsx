43fed7254ad0c34edb90a1deec069e4a6658c260
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-09-14 04:53:38 +0200

    add fillter

--- src/components/wineComponents/FilterItem.jsx
+++ src/components/wineComponents/FilterItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-export default function FilterItem({ title, mobile }) {
+export default function FilterItem({ title, mobile, categories }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -9,7 +9,7 @@ export default function FilterItem({ title, mobile }) {
       <button
         className={`flex items-center ${
           mobile ? "justify-between " : "gap-2"
-        } w-full tiny:text-[16px] text-lg text-left py-2`}
+        } w-full tiny:text-[16px] text-lg text-left py-2 justify-between`}
         onClick={() => setIsOpen(!isOpen)}
       >
         {title}
@@ -17,7 +17,13 @@ export default function FilterItem({ title, mobile }) {
           {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
         </span>
       </button>
-      {isOpen && <div className="pl-4 pt-2">...</div>}
+      {isOpen && (
+        <div className="pl-4 pt-2 flex flex-col gap-[17px]">
+          {categories?.map((item, index) => {
+            return <span key={index}>{item}</span>;
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/wineComponents/WineFilter.jsx
+++ src/components/wineComponents/WineFilter.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
+import ReactSlider from "react-slider";
 import FilterItem from "./FilterItem";
 
+const categories = ["მშრალი", "ტკბილი"];
+
 export default function WineFilter() {
+  const [sliderValues, setSliderValues] = useState([]);
+
   return (
     <div className="hidden lg:block h-96 w-[200px]">
-      <FilterItem title="ქვეყანა" />
-      <FilterItem title="წელი" />
-      <FilterItem title="წელი" />
-      <FilterItem title="წელი" />
+      <p className="text-[#78808C] text-[14px]">ფილტრი</p>
+      <p className="text-[18px] mt-[24px] text-[#141516] font-medium">ფასი</p>
+
+      <div className="mt-[20px] flex items-center justify-between">
+        <div className="flex items-center gap-[6px]">
+          <input
+            type="text"
+            defaultValue={10}
+            value={sliderValues[0]}
+            className="h-[30px] w-[60px] pl-[10px] border border-[#eaeaea] rounded-[6px]"
+          />
+          <p className="text-[#78808C] text-[14px]">ლ</p>
+        </div>
+        <div className="flex items-center gap-[6px]">
+          <input
+            type="text"
+            defaultValue={1000}
+            value={sliderValues[1]}
+            className="h-[30px] w-[60px] pl-[10px] border border-[#eaeaea] rounded-[6px]"
+          />
+          <p className="text-[#78808C] text-[14px]">ლ</p>
+        </div>
+      </div>
+      <div className="mt-[23px]">
+        <ReactSlider
+          className="horizontal-slider "
+          thumbClassName="example-thumb"
+          trackClassName="example-track"
+          defaultValue={[10, 1000]}
+          max={1000}
+          min={10}
+          onChange={(value) => setSliderValues(value)}
+        />
+      </div>
+      <div className="mt-[90px] flex flex-col gap-[21px]">
+        <FilterItem title="ტიპი" categories={categories} />
+        <FilterItem title="ფერი" categories={categories} />
+      </div>
     </div>
   );
 }
