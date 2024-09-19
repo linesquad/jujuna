f10e4c3f2a1e8e69c7cf38c0936a8a5e2aa8f054
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-09-19 14:52:26 -0700

    implement useForm
    add useForm, register and error handle, write regex, and display errors

--- src/components/checkout/ContactInformation.jsx
+++ src/components/checkout/ContactInformation.jsx
@@ -1,17 +1,59 @@
+import { DevTool } from "@hookform/devtools";
 import PaymentMethod from "./PaymentMethod";
-
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+// useRef
 const ContactInformation = () => {
+  const form = useForm({
+    defaultValues: {
+      city: "",
+      postalCode: "",
+      currentAddress: "",
+      note: "",
+    },
+  });
+  const { register, control, handleSubmit, formState, reset } = form;
+  //   setValue
+  const { errors, isSubmitSuccessful } = formState;
+  //   isSubmitting,
+  useEffect(() => {
+    if (isSubmitSuccessful) reset();
+  }, [isSubmitSuccessful, reset]);
+
+  //   const formRef = useRef(null);
+
+  const onSubmit = (data) => {
+    console.log(data); // You can handle the form data here
+  };
+
   return (
     <div className="w-full pr-5">
       <h1 className="text-lg font-bold pb-3 pt-5">Contact Information</h1>
-      <form className="w-full flex flex-col gap-2">
+      <form
+        className="w-full flex flex-col gap-2"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="w-full pb-3">
           <input
             type="text"
             id="city"
             name="city"
-            className="bg-[#EAEAEA] w-full p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
-            placeholder="City"
+            className={`bg-[#EAEAEA] w-full p-2 pl-3 border  ${
+              errors.city
+                ? "placeholder:text-red-500 border-red-500 focus:outline-none focus:ring-0"
+                : "placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
+            }`}
+            placeholder={errors.city ? errors.city.message : "City"}
+            {...register("city", {
+              required: {
+                value: true,
+                message: "City is required",
+              },
+              pattern: {
+                value: /^[A-Z][a-zA-Z]*$/,
+                message: "There is no city",
+              },
+            })}
           />
         </div>
         <div className="w-full flex gap-2">
@@ -19,8 +61,15 @@ const ContactInformation = () => {
             name="country"
             id="country"
             className="bg-[#EAEAEA] w-[50%] p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
+            defaultValue="" // Set the default value here
+            {...register("country", {
+              required: {
+                value: true,
+                message: "Country is required",
+              },
+            })}
           >
-            <option value="" disabled selected>
+            <option value="" disabled>
               Your country
             </option>
             <option value="georgia">Georgia</option>
@@ -29,8 +78,15 @@ const ContactInformation = () => {
             name="state"
             id="state"
             className="bg-[#EAEAEA] w-[50%] p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
+            defaultValue=""
+            {...register("state", {
+              required: {
+                value: true,
+                message: "State is required",
+              },
+            })}
           >
-            <option value="" disabled selected>
+            <option value="" disabled>
               Your state
             </option>
             <option value="tbilisi">Tbilisi</option>
@@ -43,8 +99,25 @@ const ContactInformation = () => {
             type="text"
             id="postalCode"
             name="postalCode"
-            className="bg-[#EAEAEA] w-full p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
-            placeholder="Postal Code"
+            className={`bg-[#EAEAEA] w-full p-2 pl-3 border  ${
+              errors.postalCode
+                ? "placeholder:text-red-500 border-red-500 focus:outline-none focus:ring-0"
+                : "placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
+            }`}
+            placeholder={
+              errors.postalCode ? errors.postalCode.message : "Postal Code"
+            }
+            maxLength={5}
+            {...register("postalCode", {
+              required: {
+                value: true,
+                message: "Postal Code is required",
+              },
+              pattern: {
+                value: /^[0-9]{1,5}$/,
+                message: "Postal Code must be numeric",
+              },
+            })}
           />
         </div>
 
@@ -53,8 +126,22 @@ const ContactInformation = () => {
             type="text"
             id="currentAddress"
             name="currentAddress"
-            className="bg-[#EAEAEA] w-full p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
-            placeholder="Current Address"
+            className={`bg-[#EAEAEA] w-full p-2 pl-3 border  ${
+              errors.currentAddress
+                ? "placeholder:text-red-500 border-red-500 focus:outline-none focus:ring-0"
+                : "placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
+            }`}
+            placeholder={
+              errors.currentAddress
+                ? errors.currentAddress.message
+                : "Current Address"
+            }
+            {...register("currentAddress", {
+              required: {
+                value: true,
+                message: "Address is required",
+              },
+            })}
           />
         </div>
 
@@ -66,15 +153,25 @@ const ContactInformation = () => {
             className="bg-[#EAEAEA] w-full p-2 pl-3 placeholder:text-[#908F8F] focus:outline-none focus:ring-0"
             placeholder="Note"
             style={{ resize: "none" }}
+            maxLength={100}
+            {...register("note", {
+              maxLength: {
+                value: 100,
+                message: "Note cannot exceed 100 characters",
+              },
+            })}
           />
         </div>
 
         <div className="flex justify-end">
           <div className="bg-[#EAEAEA] px-2 py-2 shadow-md">
-            <button className="">Address update</button>
+            <button className="" type="submit">
+              Address update
+            </button>
           </div>
         </div>
       </form>
+      <DevTool control={control} />
       <div className="pt-4">
         <PaymentMethod />
       </div>
