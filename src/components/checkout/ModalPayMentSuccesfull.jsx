10fc59c0e0fdacf6a5a73e2627ddd7e3326d9ed4
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-10-17 12:47:35 -0700

    fix modal payment succesfull

--- src/components/checkout/ModalPayMentSuccesfull.jsx
+++ src/components/checkout/ModalPayMentSuccesfull.jsx
@@ -13,37 +13,48 @@ const ModalPayMentSuccesfull = ({ orderData, handleCloseModal }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white p-10 rounded-[2.5rem] shadow-lg flex flex-col relative max-h-[100vh] ">
-        <div className="absolute top-2 right-2 cursor-pointer">
-          <IoMdClose size={60} color="grey" />
-        </div>
+      <div className="bg-white p-6 rounded-none sm:rounded-3xl shadow-lg w-full h-full sm:max-h-[90vh] sm:max-w-lg relative">
+        <button
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+          onClick={handleCloseModal}
+        >
+          <IoMdClose size={36} />
+        </button>
+
         <div className="w-full flex justify-center pb-7">
-          <div className="w-24 h-24 bg-[#E5F4ED] rounded-full border-[#613994] border-[3px] relative mt-10">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11 h-11 bg-[#613994] rounded-full">
-              <MdDone color="white" size={43} />
+          <div className="w-24 h-24 bg-[#E5F4ED] rounded-full border-[#613994] border-4 flex items-center justify-center relative mt-6">
+            <div className="w-11 h-11 bg-[#613994] rounded-full flex items-center justify-center">
+              <MdDone color="white" size={36} />
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-3 justify-center items-center">
-          <h1 className="text-[26px] font-bold text-[#474747]">
-            Payment Success!
+
+        <div className="text-center">
+          <h1 className="text-2xl font-bold text-gray-700">Payment Success!</h1>
+          <h1 className="text-4xl font-semibold text-gray-900 mt-2">
+            ${orderData.totalAmount}
           </h1>
-          <h1 className="text-[40px] font-semibold text-[#121212]">{`$${orderData.totalAmount}`}</h1>
         </div>
 
-        <hr className="mt-14 pb-6" />
+        <hr className="my-10 border-gray-300" />
 
-        <div className="grid grid-rows-2 grid-cols-2 gap-5">
+        <div className="grid grid-cols-2 gap-6">
           <ModalInfo title="Ref Number" info={orderData.phone} />
-          <ModalInfo title="Payment Time" info={orderData.createdAt} />
+          <ModalInfo
+            title="Payment Time"
+            info={new Date(orderData.createdAt).toLocaleString()}
+          />
           <ModalInfo title="Customer Name" info={orderData.username} />
           <ModalInfo title="Customer Country" info={orderData.country} />
         </div>
-        <div>
-          <Link to={`/order/${orderData._id}`} onClick={handleCloseModal}>
-            <h1 className="text-xl font-bold pt-12 pb-20 text-center">
-              Thank you!
-            </h1>
+
+        <div className="text-center mt-10">
+          <Link
+            to={`/order/${orderData._id}`}
+            onClick={handleCloseModal}
+            className="text-xl font-bold text-gray-900 hover:text-purple-600"
+          >
+            Thank you!
           </Link>
         </div>
       </div>
