6d8893e4be6755c267abad010b110b5922c5cba7
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-10-18 12:36:56 -0700

    fix import

--- src/ui/ReusableErrorRobot.jsx
+++ src/ui/ReusableErrorRobot.jsx
@@ -1,10 +1,10 @@
 import Lottie from "lottie-react";
-import grapeLoader from "../animation/errorRobot.json";
+import errorRobot from "../animation/errorRobot.json";
 
 const ReusableErrorRobot = ({ width, height }) => {
   return (
     <Lottie
-      animationData={grapeLoader}
+      animationData={errorRobot}
       loop={true}
       autoplay={true}
       style={{ width, height }}
